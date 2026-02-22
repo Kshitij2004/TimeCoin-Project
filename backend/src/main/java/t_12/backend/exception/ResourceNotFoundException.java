3d34a5cab99bfb892d286141ba6d3a8c903afabb
dsweston <dsweston@wisc.edu>
2026-02-21 22:22:56 -0600

    Added resource not fount exception handler, returns JSON

---
+++ backend/src/main/java/t_12/backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package t_12.backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}

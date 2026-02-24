71e8a9ef1bc212941897f9a0da666898f751cda0
gpundir <gpundir@wisc.edu>
2026-02-24 11:25:17 -0600

    added frontend for register.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+// Create an instance to avoid repeating the base URL
+const api = axios.create({
+    baseURL: 'http://localhost:8080/api', // Your Java Spring Boot address
+});
+
+export const registerUser = (userData) => {
+    return api.post('/register', userData);
+};
+
+export default api;

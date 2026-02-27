be8e89eb9484bbb9873578b7dd2fb26bf6fdb0f9
dsweston <dsweston@wisc.edu>
2026-02-27 10:41:14 -0600

    Added user DTO

---
+++ backend/src/main/java/t_12/backend/api/auth/UserDTO.java
@@ -0,0 +1,36 @@
+package t_12.backend.api.auth;
+
+import java.time.LocalDateTime;
+
+import t_12.backend.entity.User;
+
+public class UserDTO {
+
+    private Long id;
+    private String username;
+    private String email;
+    private LocalDateTime createdAt;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.createdAt = user.getCreatedAt();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}

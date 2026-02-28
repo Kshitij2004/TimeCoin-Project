5452d26a2a989319670a7ef844ad0fd9abd77508
Jeremiah Jin <zjin254@wisc.edu>
2026-02-28 12:23:54 -0600

    Fix for pipeline

--- frontend/src/user_transaction_history/HistoryPage.test.js
+++ frontend/src/user_transaction_history/HistoryPage.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import HistoryPage from './HistoryPage';
 
 beforeEach(() => {
